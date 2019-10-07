dd9ff2c785220bce4be21c1a54ecab996c0493c4
machine100 <joesmileyyy@yahoo.com>
2019-10-06 18:01:07 -0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridpatternComponent } from './components/gridpattern/gridpattern.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridpatternComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/gridpattern/gridpattern.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gridpattern',
+  templateUrl: './gridpattern.component.html',
+  styleUrls: ['./gridpattern.component.scss']
+})
+export class GridpatternComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
